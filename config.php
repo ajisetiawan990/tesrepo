ceffe5e08df0c372774b3dfd8d363bad870421cf
ajisetiawan990 <ajidec10@gmail.com>
2025-10-16 09:37:25 +0700

    perubahan config

--- config.php
+++ config.php
@@ -6,10 +6,3 @@ $pass = "";
 $db = "data_sekolah";
 
 $conn = mysqli_connect($host, $user, $pass,$db);
-
-// Mengecek koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-
-echo "Koneksi berhasil!";
--- ubahsiswa.php
+++ ubahsiswa.php
@@ -5,7 +5,7 @@ if (isset($_POST['update'])) {
     $id        = $_POST['id'];
     $nama      = $_POST['nama'];
     $jurusan   = $_POST['jurusan'];
-    $kelas  = $_POST['kelas'];
+    $kelas     = $_POST['kelas'];
 
     $query = "UPDATE siswaa 
               SET nama_siswa='$nama', jurusan='$jurusan', kelas='$kelas'
